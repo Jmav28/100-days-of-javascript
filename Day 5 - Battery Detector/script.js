586d6692641f418e16770262a641de48a53af606
Miguel Avila <miguelavilaa28@gmail.com>
2023-03-06 22:39:36 -0800

    Proyecto del día 3 terminado

---
+++ Day 5 - Battery Detector/script.js
@@ -0,0 +1,44 @@
+initBattery();
+
+function initBattery() {
+    const batteryLiquid = document.querySelector(".Bliquid");
+    const batteryStatus = document.querySelector(".Bstatus");
+    const Bpercentage = document.querySelector(".Bpercentage");
+    navigator.getBattery().then((batt) => {
+        updateBattery = () => {
+            let level = Math.floor(batt.level * 100);
+            Bpercentage.innerHTML = level + "%";
+            batteryLiquid.style.height = `${parseInt(batt.level * 100)}%`;
+            console.log(`${parseInt(batt.level * 100)} %`);
+            if(level == 100) {
+                batteryStatus.innerHTML = `Battery Full < class="ri-battery-2-fill green-color"></i>`;
+                batteryLiquid.style.height = "103%";
+            } else if(level <= 20 &! batt.charging) {
+                batteryStatus.innerHTML = `Low Charge < class="ri-plug-line animated-red animated-red"></i>`;
+            } else if(batt.charging) {
+                batteryStatus.innerHTML = `Charging... < class="ri-flashlight-line animated-green"></i>`;
+            } else {
+                batteryStatus.innerHTML="";
+            }
+
+            if(level <=20) {
+                batteryLiquid.classList.add("gradient-color-red");
+                batteryLiquid.classList.remove("gradient-color-green", "gradient-color-orange", "gradient-color-yellow");
+            }else if(level <=48) {
+                batteryLiquid.classList.add("gradient-color-orange");
+                batteryLiquid.classList.remove("gradient-color-green", "gradient-color-red", "gradient-color-yellow");
+            }
+            if(level <=80) {
+                batteryLiquid.classList.add("gradient-color-yellow");
+                batteryLiquid.classList.remove("gradient-color-green", "gradient-color-orange", "gradient-color-red");
+            } else {
+                batteryLiquid.classList.add("gradient-color-green");
+                batteryLiquid.classList.remove("gradient-color-yellow", "gradient-color-orange", "gradient-color-red");
+            }
+        }
+        updateBattery();
+        batt.addEventListener("chargingchange", () => {updateBattery();});
+        batt.addEventListener("levelchange", () => {updateBattery();});
+        
+    });
+}
